6e855c64165e6d82b322b16932d71533c267c59a
unknown <kartiksarwan2017.comp@mmcoe.edu.in>
2022-08-24 10:55:32 +0530

    Action creators

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
+import { ADD_MOVIES } from '../actions';
+
 export default function movies(state = [], action){
 
-    if(action.type === 'ADD_MOVIES'){
+    if(action.type === ADD_MOVIES){
         return action.movies;
     }
 
     return state;
 
 }
+

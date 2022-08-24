a77363692c08009d688c9ed305cd4ad2992a8611
unknown <kartiksarwan2017.comp@mmcoe.edu.in>
2022-08-24 09:09:07 +0530

    Adding reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
-{
-    type: 'INCREASE_COUNT'
-}
+// {
+//     type: 'ADD_MOVIES',
+//     movies: [m1, m2, m3]
+    
+// }
 
-{
-    type: 'DECREASE_COUNT'
-}
+// {
+//     type: 'DECREASE_COUNT'
+// }
